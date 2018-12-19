800775907f13622ef37a6da611538d47323ffb7f
Morgan <nicol.morgan@outlook.fr>
2018-12-19 17:16:20 +0100

    niketoi

--- application/views/profile.php
+++ application/views/profile.php
@@ -11,6 +11,12 @@
 			<p class="nom">Nom : {google.nom}</p>
 			<p class="tarif">Tarif horaire : {user.tarif}</p>
 		</div>
+		<form action="" method="post">
+			<label for="tarif">Tarif/horaire</label>
+			<input type="text" id="tarif">
+			<label for="description">Description</label>
+			<input type="text" id="description">
+		</form>
 	</section>
 </main>
 
