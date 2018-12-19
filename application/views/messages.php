6cfcaa93409b4d89390de44e4f3c8e99a7dea07f
Morgan <nicol.morgan@outlook.fr>
2018-12-19 12:27:09 +0100

    Controllers and path for each views

--- application/views/messages.php
+++ application/views/messages.php
@@ -1,7 +1,6 @@
 <?php $this->load->view('shared/header'); ?>
 <?php $this->load->view('shared/menubar'); ?>
 
-
 <main class="page landing-page"></main>
 
 <?php $this->load->view('shared/footer');?>
