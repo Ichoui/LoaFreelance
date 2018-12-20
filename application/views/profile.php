ece56292aef788cef90dd4da90be5d4dc2cdca4b
Morgan <nicol.morgan@outlook.fr>
2018-12-20 11:35:40 +0100

    fix responsive

--- application/views/profile.php
+++ application/views/profile.php
@@ -24,14 +24,15 @@
 	<hr>
 	<div class="bottom">
 		<p class="text-center">Mettez à jours vos informations dans les champs ci-dessous</p>
+		<div class="container">
 		<div class="row">
 			<div class="col-md-6">
 				<form id="form-tarif" action="" method="post" class="form-group">
 					<div class="form-group">
 						<label for="tarif">Tarif/horaire</label>
 						<input type="number" step="0.5" class="form-control" id="tarif" placeholder="Votre tarif/horaire ...">
+						<button type="submit" class="btn btn-outline-primary"></button>
 						<small class="form-text text-muted">Exprimé en € /h</small>
-						<button type="submit" class="btn btn-outline-primary">Mettre à jour</button>
 					</div>
 				</form>
 			</div>
@@ -45,8 +46,8 @@
 							<option>Daesh</option>
 							<option>Danseur-étoile</option>
 						</select>
+						<button type="submit" class="btn btn-outline-primary"></button>
 						<small class="form-text text-muted">Quel métier exercez-vous</small>
-						<button type="submit" class="btn btn-outline-primary">Mettre à jour</button>
 					</div>
 				</form>
 			</div>
@@ -57,8 +58,8 @@
 					<div class="form-group">
 						<label for="cv">Curriculum Vitae</label>
 						<input type="file" class="form-control" id="cv" accept="application/pdf">
+						<button type="submit" class="btn btn-outline-primary"></button>
 						<small class="form-text text-muted">Uploadez votre CV</small>
-						<button type="submit" class="btn btn-outline-primary">Mettre à jour</button>
 					</div>
 				</form>
 			</div>
@@ -67,10 +68,11 @@
 					<div class="form-group">
 						<label for="description">Description</label>
 						<textarea type="text" class="form-control" id="description" placeholder="Votre description ..."></textarea>
+						<button type="submit" class="btn btn-outline-primary"></button>
 					</div>
-						<button type="submit" class="btn btn-outline-primary">Mettre à jour</button>
 				</form>
 			</div>
+		</div>
 		</div>
 	</div>
 </main>
