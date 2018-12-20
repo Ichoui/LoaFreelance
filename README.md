1a10b82fb679861733501fa97287e2429c59e090
Morgan <nicol.morgan@outlook.fr>
2018-12-20 10:16:20 +0100

    OK !

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 ### Installation
 
 ``npm i`` => Installe les dépendances 
-<br> Si ne fonctionne pas :
+``npm i -g gulp@3.9.1``<br>
 ``npm i --save gulp@3.9.1 bootstrap-sass gulp-sass gulp-sourcemaps``
-
-### Lancer le front
-
-``gulp`` => Permet de compiler le sass en css. A faire à la racine du projet (.../LoaFreelance)
-
-``application``  Contient le projet
-
-``application/views``  Contient toutes les assets (js, images, css et scss)
 
 ### Config
 Le fichier application/config/<b>config.php</b> <u>n'est pas gitté</u>.<br>
 Un fichier "template" application/config/c<b>onfig-default.php</b>	 est présent pour faire suivre les informations. <br>
 Lorsqu'on effectue une modification sur notre config.php local, <u>ON DOIT</u> faire la modification dans <b>config-default.php</b>	.
+
+
+### Lancer le front
+``gulp`` => Permet de compiler le sass en css. A faire à la racine du projet (.../LoaFreelance)
+
+### Projet
+``application``  Contient le projet
+
+``application/views``  Contient toutes les assets (js, images, css et scss)
+
+
 
--- application/views/profile.php
+++ application/views/profile.php
@@ -3,35 +3,39 @@
 
 <main class="page profile ">
 	<div class="row">
-	<div class="left col  col-md-4">
-		<img src="http://api.ichoui.fr/maple/IMG_7293.JPG">
-	</div>
-	<div class="right col-md-6">
-		<div class="identity">
-			<p class="prenom"><span>Prénom : </span>{google.prenom}</p>
-			<p class="nom"><span>Nom :</span> {google.nom}</p>
-			<p class="tarif"><span>Tarif/horaire :</span> {user.tarif}</p>
-			<p class="description"><span>Description :</span> {user.description}</p>
-<!--			<p class="tarif"><span>Tarif/horaire :</span> {user.tarif}</p>-->
+		<div class="left col  col-md-4">
+			<img src="http://api.ichoui.fr/maple/IMG_7293.JPG">
 		</div>
-		<hr>
-
-
-
-		<form action="" method="post" class="form-group">
-			<div class="form-group">
-				<label for="tarif">Tarif/horaire</label>
-				<input type="number" step="0.5" class="form-control" id="tarif" placeholder="Votre tarif/horaire ...">
-				<small class="form-text text-muted">Exprimé en € /h</small>
+		<div class="right col-md-6">
+			<div class="identity">
+				<p class="prenom"><span>Prénom : </span>{google.prenom}</p>
+				<p class="nom"><span>Nom :</span> {google.nom}</p>
+				<hr>
+				<p class="tarif"><span>Tarif/horaire :</span> {user.tarif}</p>
+				<p class="description"><span>Description :</span> {user.description}</p>
+				<!--			<p class="tarif"><span>Tarif/horaire :</span> {user.tarif}</p>-->
+				<p class="cv"><span>Curriculum Vitae :</span></p>
 			</div>
-			<div class="form-group">
-				<label for="description">Description</label>
-				<input type="email" class="form-control" id="description" placeholder="Votre description ...">
-				<small class="form-text text-muted">Une petite description de vous ?</small>
-			</div>
-			<button class="btn btn-outline-primary">Mettre à jour</button>
-		</form>
+
+		</div>
 	</div>
+	<hr>
+	<div class="row">
+		<div class="bottom">
+			<form action="" method="post" class="form-group">
+				<div class="form-group">
+					<label for="tarif">Tarif/horaire</label>
+					<input type="number" step="0.5" class="form-control" id="tarif" placeholder="Votre tarif/horaire ...">
+					<small class="form-text text-muted">Exprimé en € /h</small>
+				</div>
+				<div class="form-group">
+					<label for="description">Description</label>
+					<input type="email" class="form-control" id="description" placeholder="Votre description ...">
+					<small class="form-text text-muted">Une petite description de vous ?</small>
+				</div>
+				<button class="btn btn-outline-primary">Mettre à jour</button>
+			</form>
+		</div>
 	</div>
 </main>
 
