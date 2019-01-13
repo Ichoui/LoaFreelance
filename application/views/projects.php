5e6e11be84392be4e20968ef7959de243f3fb8e7
ichoui <nicol.morgan@outlook.fr>
2019-01-13 17:30:35 +0100

    Création de la vue avec le chat

--- application/views/projects.php
+++ application/views/projects.php
@@ -4,9 +4,22 @@
 <main class="page projects">
 	<section>
 		<h1 class="main-title">{Projet.name}</h1>
+		<div class="about">
+			<div class="description"><span>Description :</span> Lorem ipsum dolor sit amet, consectetur adipisicing
+				elit. Aut magnam soluta
+				voluptate? Delectus doloremque libero nihil suscipit temporibus voluptate voluptatem.
+			</div>
+			<div class="proprietaire"><span>Porteur du projet : </span>{Nom Prenom}</div>
+			<div class="free"><span>Freelanceur(s) inscrits :</span>
+				<ul>
+					<li>Free A</li>
+					<li>Free B</li>
+				</ul>
+			</div>
+		</div>
 	</section>
 </main>
-
+<?php $this->load->view('shared/chat-window'); ?>
 <?php $this->load->view('shared/footer'); ?>
 
 
---
+++ application/views/shared/chat-window.php
@@ -0,0 +1,3 @@
+<div class="chat-window" style="position:absolute; bottom:0; right: 0;">
+	Je suis un chat<br>mais j'ai une sale gueule
+</div>
