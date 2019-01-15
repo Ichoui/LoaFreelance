3a0637e5c75501e10ded1c32f98412d8a8f9f744
JeyZu <jeremie.quatorze@gmail.com>
2019-01-15 14:26:11 +0100

    login page

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,11 +3,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller
 {
+	public function __construct()
+  {
+  	parent::__construct();
+    $this->load->model('user');
+		$this->load->helper('url');
+		$this->load->helper('form');
+
+  }
 
 	public function index()
 	{
-		$this->load->helper('url');
-		$data['place'] = 'login';
+
+
+		$data = [
+				'place' => 'login',
+		];
 		$this->load->view('notlogged/login', $data);
+	}
+
+	public function connexion() {
+
+			var_dump($this->input->post('email'));
+			$login = $this->user->get_connected_user();
+
+			if(!empty($login))
+				$this->load->view('profile');
+
+			$this->index();
 	}
 }
--- application/views/notlogged/login.php
+++ application/views/notlogged/login.php
@@ -8,15 +8,18 @@
 				<h2 class="text-info">Connexion</h2>
 				<p>Choississez la solution qui vous convient ci-dessous pour vous connecter.</p>
 			</div>
-			<form class="text-center">
-				<button class="btn btn-primary" type="button" style="background-color: #0073b1;margin-bottom: 10px;">Linkedin Connexion</button>
-				<p class="text-center">ou</p>
-				<div class="form-group"><input class="form-control item" type="email" placeholder="Email" id="email"></div>
-				<input class="form-control" type="password" placeholder="Mot de passe" id="password">
-				<button class="btn btn-primary btn-block" type="submit" style="background-color: #49c5b6;margin-top: 10px;">Connexion</button>
-				<p style="margin: 15px 0;">Pas encore inscrit ?</p>
-				<a class="btn btn-primary btn-block" role="button" href="<?= base_url('registration') ?>" style="background-color: #49c5b6;margin-top: 10px;">S'enregistrer</a>
-			</form>
+			<?= validation_errors(); ?>
+			<div class="text-center">
+				<?= form_open('login/connexion') ?>
+					<button class="btn btn-primary" type="button" style="background-color: #0073b1;margin-bottom: 10px;">Linkedin Connexion</button>
+					<p class="text-center">ou</p>
+					<div class="form-group"><input class="form-control item" type="email" name="email" placeholder="Email" id="email"></div>
+					<input class="form-control" type="password" name="password" placeholder="Mot de passe" id="password">
+					<input class="btn btn-primary btn-block" type="submit"  name="submit" style="background-color: #49c5b6;margin-top: 10px;" value="Connexion" />
+					<p style="margin: 15px 0;">Pas encore inscrit ?</p>
+					<a class="btn btn-primary btn-block" role="button" href="<?= base_url('registration') ?>" style="background-color: #49c5b6;margin-top: 10px;">S'enregistrer</a>
+				</form>
+			</div>
 		</div>
 	</section>
 </main>
