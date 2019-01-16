8178c80b3e619a5dbf464596c7ce06caaa40d5f4
destefani.yoann <ydestefa.destefani.yoann@gmail.com>
2019-01-16 10:09:01 +0100

    dynamic messagerie

--- application/views/messages.php
+++ application/views/messages.php
@@ -4,27 +4,27 @@
 <main class="page landing-page messages">
 	<section class="container">
 		<div class="email-part">
+			<?php
+				foreach ($messages as $row) {
 
-			<div class="mail">
-				<div class="expediteur">expediteur</div>
-				<div class="title">Titre</div>
-			</div>
-			<div class="mail">
-				<div class="expediteur">expediteur</div>
-				<div class="title">Titre</div>
-			</div>
-			<div class="mail">
-				<div class="expediteur">expediteur</div>
-				<div class="title">Titre</div>
-			</div>
+					$date = new DateTime($row->date_send);					
+
+					echo'<div id="message_'.$row->id.'" class="mail">
+						 	<div class="expediteur">'.$row->first_name.' '.$row->last_name.' | '.$date->format('d/m/Y H:i').'</div>
+						    <div class="title">'.$row->title.'</div>
+						 </div>';
+				}
+			?>
 		</div>
+		
 		<div class="corps-text-part">
+			<!--
 			<div class="header">
-				<div class="expediteur">Nom de l'expediteur</div>
-				<div class="title">Mon titre c'est de la daube</div>
+				<div id="title" class="expediteur">Nom de l'expediteur</div>
+				<div id="object" class="title">Mon titre c'est de la daube</div>
 				<div class="repondre">Répondre</div>
 			</div>
-			<div class="message">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur blanditiis cumque debitis dignissimos distinctio dolor dolorem eos excepturi explicabo illo maiores minima molestiae nam natus nemo nihil non omnis optio placeat quasi quis .</div>
+			<div id="message" class="message">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur blanditiis cumque debitis dignissimos distinctio dolor dolorem eos excepturi explicabo illo maiores minima molestiae nam natus nemo nihil non omnis optio placeat quasi quis .</div>
 			<div class="reponse">
 				<div class="close-reponse">Retourner au message</div>
 				<form action="" type="POST">
@@ -32,9 +32,48 @@
 					<input type="file" multiple="multiple">
 					<button class="btn btn-outline-primary">Envoyer</button>
 				</form>
-			</div>
-
+			</div> -->
 		</div>
+	
 	</section>
 </main>
 <?php $this->load->view('shared/footer'); ?>
+
+<!-- MOVE CALL AJAX & JQUERY -->
+
+<script>
+$(document).ready(function(){
+
+ function load_data(id)
+ {
+  $.ajax({
+   url:"<?php echo base_url(); ?>messages/fetch",
+   method:"POST",
+   data:{id:id},
+   success:function(data){
+    $('.corps-text-part').html(data);
+   }
+  })
+ }
+
+
+$('[id^=message_]').on('click',function(){
+	var id = $(this).attr('id').replace("message_","");
+	load_data(id);
+});
+/*
+ $('#search_text').keyup(function(){
+  var search = $(this).val();
+  if(search != '')
+  {
+   load_data(search);
+  }
+  else
+  {
+   load_data();
+  }
+ });*/
+});
+</script>
+
+
