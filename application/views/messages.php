155dd982e3c003ed8917cc104fe1c723248099e6
Morgan <nicol.morgan@outlook.fr>
2019-03-02 15:43:22 +0100

    messages seems ok right now!

--- application/views/messages.php
+++ application/views/messages.php
@@ -5,25 +5,24 @@
 	<section class="container">
 		<div class="email-part">
 			<?php
-				$HasResults = FALSE;
-				
-				foreach ($messages as $row) {
+			$HasResults = FALSE;
 
-					$date = new DateTime($row->date_send);					
+			foreach ($messages as $row) {
 
-					echo'<div id="message_'.$row->id.'" class="mail">
-						 	<div class="expediteur">'.$row->first_name.' '.$row->last_name.' | '.$date->format('d/m/Y H:i').'</div>
-						    <div class="title">'.$row->title.'</div>
+				$date = new DateTime($row->date_send);
+
+				echo '<div id="message_' . $row->id . '" class="mail">
+						 	<div class="expediteur">' . $row->first_name . ' ' . $row->last_name . ' | ' . $date->format('d/m/Y H:i') . '</div>
+						    <div class="title">' . $row->title . '</div>
 						 </div>';
-						 $HasResults = TRUE;
-				}
-				if($HasResults == FALSE)
-				{
-					echo'<p>Boite mail vide</p>';
-				}
+				$HasResults = TRUE;
+			}
+			if ($HasResults == FALSE) {
+				echo '<p>Boite mail vide</p>';
+			}
 			?>
 		</div>
-		
+
 		<div class="corps-text-part">
 			<div class="header">
 				<div id="title" class="expediteur"></div>
@@ -38,30 +37,30 @@
 					<input type="file" multiple="multiple">
 					<button name="submit" class="btn btn-outline-primary">Envoyer</button>
 				</form>
-			</div> 
+			</div>
 		</div>
-		
+
 
 		<!-- Ecrire un nouveau message à designer :p -->
-
-	<!--form_open('messages/sendMessage'); -->
-	<form id="form_message">
-	  		<div class="form-group">		    
-		    <input type="email" id="input_email" placeholder="@Destinataire">
-		  </div>
-		  <div class="form-group">
-		    <input  type="text" class="form-control" id="input_titre" placeholder="Titre">
-		  </div>
-		  <div class="form-group">
-		    <input type="text" class="form-control" id="input_objet" placeholder="Object">
-		  </div>
-		  <div class="form-group">
-		  <textarea class="form-control" rows="5" id="input_message" placeholder="message"></textarea>
-		  </div>
-		  <button name="submit" id="btn_send_msg"class="btn btn-primary">Envoyé</button>
-	<!--//form_close(); -->
-</form>
-<div id="statut_send"></div>
+		<?php
+		echo form_open('messages/sendMessage');
+		echo '<form id="form_message">';
+		echo '<div class="form-group">		 ';
+		echo '<input type="email" id="input_email" placeholder="@Destinataire">';
+		echo '</div>';
+		echo '<div class="form-group">';
+		echo '<input  type="text" class="form-control" id="input_titre" placeholder="Titre">';
+		echo '</div>';
+		echo '<div class="form-group">';
+		echo '  <input type="text" class="form-control" id="input_objet" placeholder="Object">';
+		echo '</div>';
+		echo '<div class="form-group">';
+		echo ' <textarea class="form-control" rows="5" id="input_message" placeholder="message"></textarea>';
+		echo ' </div>';
+		echo ' <button name="submit" id="btn_send_msg" class="btn btn-outline-primary">Envoyé</button>';
+		echo '</form>';
+		form_close(); ?>
+		<div id="statut_send"></div>
 		<!-- Fin du btn -->
 	</section>
 </main>
@@ -70,92 +69,94 @@
 <!-- MOVE CALL AJAX & JQUERY -->
 
 <script>
-$(document).ready(function(){
+	$(document).ready(function () {
 
-$('#form_message').submit(function(event) {
-                     
-               var formData = {
-                   'destinataire'              : $('#input_email').val(),
-                   'titre'                     : $('#input_titre').val(),
-                   'objet'                     : $('#input_objet').val(),
-                   'message'				   : $('#input_message').val()
-               };
-   
-               // process the form
-               $.ajax({
-                   type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-                   url         : "<?php echo base_url();?>" + "messages/sendMessage", // the url where we want to POST
-                   data        : formData, // our data object
-                   dataType    : 'json', // what type of data do we expect back from the server
-                   encode      : true
-               })
-                   // using the done promise callback
-                   .done(function(data) {
-   
-                       // log data to the console so we can see
-                       console.log(data); 
-   
-                       // here we will handle errors and validation messages
-                   });
-   
-               // stop the form from submitting the normal way and refreshing the page
-               event.preventDefault();
-    
+		$('#form_message').submit(function (event) {
+
+			var formData = {
+				'destinataire': $('#input_email').val(),
+				'titre': $('#input_titre').val(),
+				'objet': $('#input_objet').val(),
+				'message': $('#input_message').val()
+			};
+
+			// process the form
+			$.ajax({
+				type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+				url: "<?php echo base_url();?>" + "messages/sendMessage", // the url where we want to POST
+				data: formData, // our data object
+				dataType: 'json', // what type of data do we expect back from the server
+				encode: true
+			})
+			// using the done promise callback
+				.done(function (data) {
+
+					// log data to the console so we can see
+					console.log(data);
+
+					// here we will handle errors and validation messages
+				});
+
+			// stop the form from submitting the normal way and refreshing the page
+			event.preventDefault();
+
 		});
-});
-$(function(){
-	$('#form_message').on('submit', function (e){
-		e.preventDefault();
-		sendFromMessage($('#input_email').val(),$('#input_titre').val(),$('#input_objet').val(),$('#input_message').val());
+	});
+	$(function () {
+		$('#form_message').on('submit', function (e) {
+			e.preventDefault();
+			sendFromMessage($('#input_email').val(), $('#input_titre').val(), $('#input_objet').val(), $('#input_message').val());
+		})
 	})
-})
-	
-function sendFromMessage(destinataire,titre,objet,message)
-{
-	$.ajax({
-		url:"<?php //echo base_url(); ?>messages/sendMessage",
-		method:"POST",
-		data:{destinataire:destinataire,
-			 titre:titre,
-			 objet:objet,
-			 message:message
-		},
-		//dataType: 'JSON',
-		success:function(data){
-			//$('#statut_send').empty();
-			$('#statut_send').html(data);
-			$('#statut_send').fadeIn();
-			$("#statut_send").fadeOut(2000, function() { $(this).empty();});
-		}
-	})
-	return false;
-}
- function load_data(id)
- {
-  $.ajax({
-   url:"<?php //echo base_url(); ?>messages/fetch",
-   method:"POST",
-   data:{id:id},
-   dataType: 'JSON',
-   success:function(data){ 	
-    //$('.corps-text-part').html(data.id_expediteur);
-    $('#title').html(data.title);
-    $('#object').html(data.object);
-    $('#message').html(data.message);
-   }
-  })
- }
 
-	$('[id^=message_]').on('click',function(){
-		var id = $(this).attr('id').replace("message_","");
+	function sendFromMessage(destinataire, titre, objet, message) {
+		$.ajax({
+			url: "<?php //echo base_url(); ?>messages/sendMessage",
+			method: "POST",
+			data: {
+				destinataire: destinataire,
+				titre: titre,
+				objet: objet,
+				message: message
+			},
+			//dataType: 'JSON',
+			success: function (data) {
+				//$('#statut_send').empty();
+				$('#statut_send').html(data);
+				$('#statut_send').fadeIn();
+				$("#statut_send").fadeOut(2000, function () {
+					$(this).empty();
+				});
+			}
+		})
+		return false;
+	}
+
+	function load_data(id) {
+		$.ajax({
+			url: "<?php //echo base_url(); ?>messages/fetch",
+			method: "POST",
+			data: {id: id},
+			dataType: 'JSON',
+			success: function (data) {
+				//$('.corps-text-part').html(data.id_expediteur);
+				$('#title').html(data.title);
+				$('#object').html(data.object);
+				$('#message').html(data.message);
+			}
+		})
+	}
+
+	$('[id^=message_]').on('click', function () {
+		var id = $(this).attr('id').replace("message_", "");
 		load_data(id);
 	});
-	
 
-	$('#btn_send_msg').on('click', function(){
-		sendFromMessage($('#input_email').val(),$('#input_titre').val(),$('#input_objet').val(),$('#input_message').val());
+
+	$('#btn_send_msg').on('click', function () {
+		sendFromMessage($('#input_email').val(), $('#input_titre').val(), $('#input_objet').val(), $('#input_message').val());
 	});
-	
+
 
 </script>
 
