18c5da7491f13ffa65ede9ff374aa1c457473d17
Morgan <nicol.morgan@outlook.fr>
2019-03-03 21:51:44 +0100

    Ajout du blabla de SEO & favicon

--- application/views/shared/header.php
+++ application/views/shared/header.php
@@ -4,9 +4,12 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-	<title>Freelancer.com | Site de Freelances</title>
+	<title>Freelancer.com | Site de Freelanceur</title>
 	<meta name="description"
-		  content="LOAFreelancer est une plateforme de mise en relation entre prestataires freelances et clients potentiels.Et est également une plateforme intégrée pour faire le suivi des projets et collaborations entamées via le site.Pour résumer, LOAFreelancer permet de gérer la collaboration de A à Z, de la mise en relation jusqu’au suivi des réalisations et le paiement.">
+		  content="Freelancer est une plateforme de mise en relation entre prestataire freelances et clients potentiels. Freelanceur.com est également une
+		  plateforme intégrée pour faire le suivi des projets et collaborations entamées via le site. Pour résumer, LOAFreelancer permet de gérer la
+		  collaboration de A à Z, de la mise en relation jusqu’au suivi des réalisations et le paiement sur un projet. Freelancer.com regroupe une vaste
+		  communauté de Freelanceurs Français.">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
@@ -14,6 +17,7 @@
 	<link rel="stylesheet" href="<?= base_url('assets/css/smoothproducts.css') ?>">
 	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css')?>">
 	<link rel="stylesheet" href="<?= base_url('assets/css/app.css') ?>">
+	<link rel="icon" href="<?= base_url('assets/img/Logo_Favicon.jpg') ?>">
 
 </head>
 
